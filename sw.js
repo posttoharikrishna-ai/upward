5f527f172d394021dca37e0b2bee216a4ff65add
posttoharikrishna-ai <posttoharikrishna@gmail.com>
2026-09-26 12:36:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,40 +8,26 @@ const APP_FILES = [
 ];
 
 self.addEventListener("install", event => {
-
     event.waitUntil(
-
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(APP_FILES))
             .then(() => self.skipWaiting())
-
     );
-
 });
 
-
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
         caches.keys()
             .then(keys => {
-
                 return Promise.all(
-
                     keys
                         .filter(key => key !== CACHE_NAME)
                         .map(key => caches.delete(key))
-
                 );
-
             })
             .then(() => self.clients.claim())
-
     );
-
 });
-
 
 self.addEventListener("fetch", event => {
 
@@ -50,7 +36,6 @@ self.addEventListener("fetch", event => {
     }
 
     event.respondWith(
-
         caches.match(event.request)
             .then(cached => {
 
@@ -79,12 +64,7 @@ self.addEventListener("fetch", event => {
                         return response;
 
                     })
-                    .catch(() =>
-                        caches.match("./index.html")
-                    );
-
+                    .catch(() => caches.match("./index.html"));
             })
-
     );
-
 });
